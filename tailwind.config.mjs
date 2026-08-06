dd587bce22f964986e8f98ab486caf6272c54ca4
javaair <appjavadev2019@gmail.com>
2026-08-06 11:51:56 -0500

    change portfolio to studio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  'app-bg': '#09090b',
+		  'app-card': '#18181b',
+		  'app-accent': '#a855f7',
+		}
+	  },
 	},
 	plugins: [],
-}
+  }
